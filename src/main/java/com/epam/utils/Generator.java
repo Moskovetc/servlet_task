7efe19a76ff4cb6c800ced537b1bc7ba81345814
moskovetc <moskovetc@hotmail.com>
2018-12-25 16:33:34 +0400

    ned to do index

--- src/main/java/com/epam/utils/Generator.java
+++ src/main/java/com/epam/utils/Generator.java
@@ -2,29 +2,29 @@ package com.epam.utils;
 
 import com.epam.entity.Product;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-//          "Product{" +
-//        "brand='" + brand + '\'' +
-//        ", model='" + model + '\'' +
-//        ", price=" + price +
-//        ", id=" + id +
-//        ", imgSrc='" + imgSrc + '\'' +
-
 public class Generator {
-//    private List<String> brands = Arrays.asList("LG", "SONY");
-//    private List<String> models = Arrays.asList("Galaxy", "Sonic");
-//    public List<Product> getProducts(int quantity) {
-//        Random random = new Random();
-//        for (int i = 0; i < quantity; i++) {
-//            Product product = new Product();
-//            product.setBrand(brands.get(random.nextInt(brands.size())));
-//            product.setModel(models.get(String.format("%s %s", , random.nextInt(1000))));
-//            product.setPrice(random.nextInt(20000));
-//            product.setId(i);
-//        }
-//
-//    }
+    private List<String> brands = Arrays.asList("LG", "SONY");
+    private List<String> models = Arrays.asList("Galaxy", "Sonic");
+    private List<String> imgs = Arrays.asList("resources/imgs/smartphone.png", "resources/imgs/headphone.png");
+
+    public List<Product> getProducts(int quantity) {
+        Random random = new Random();
+        List<Product> products = new ArrayList<>();
+        for (int i = 0; i < quantity; i++) {
+            Product product = new Product();
+            product.setBrand(brands.get(random.nextInt(brands.size())));
+            product.setModel(String.format("%s %s", models.get(random.nextInt(models.size())),
+                    random.nextInt(1000)));
+            product.setPrice(random.nextInt(20000));
+            product.setId(i);
+            product.setImgSrc(imgs.get(random.nextInt(imgs.size())));
+            products.add(product);
+        }
+        return products;
+    }
 }
